d8b0211cca2c77b290646d1fa043ea94627383d3
Amr Ragab <38997602+amrragab8080@users.noreply.github.com>
2022-06-01 12:00:17 -0400

    refactor event parser

--- nvidia-efa-ami_base/cloudwatch/nvidia/aws-hwaccel-event-parser.py
+++ nvidia-efa-ami_base/cloudwatch/nvidia/aws-hwaccel-event-parser.py
@@ -1,13 +1,9 @@
 import subprocess
 import platform
 
-plat=platform.platform()
-if 'Ubuntu' in plat:
-    f = subprocess.Popen(['tail','-F','-n','1000','/var/log/syslog'],\
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-else:
-    f = subprocess.Popen(['tail','-F','-n','1000','/var/log/messages'],\
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+f = subprocess.Popen(['tail','-F','-n','1000','/var/log/dmesg'],\
+    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 while True:
     line = f.stdout.readline()
